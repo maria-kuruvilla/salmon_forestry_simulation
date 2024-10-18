ab4933307a67adeccd77a15c518cb6aff1da6540
maria-kuruvilla <maria75799@gmail.com>
2024-10-17 17:25:11 -0700

    added correlation plots and dataset with sst

--- stan_models/SST.R
+++ stan_models/SST.R
@@ -51,3 +51,43 @@ chum_data_w_coord <- chum_data %>%
 
 write.csv(chum_data_w_coord, here("data","chum_SR_20_hat_yr_w_coord.csv"))
 
+#read data with SST data
+
+chum_sst <- read.csv(here("data","chum_SR_20_hat_yr_w_coord_w_SSTCOBE2.csv"))
+
+#make correlation plot using ggpairs between sst, npgo, eca values
+
+library(GGally)
+
+cu_list <- ch20r %>% 
+  group_by(CU) %>% 
+  summarise(n = n_distinct(River)) %>% 
+  arrange(n) %>% 
+  filter(n>10) %>% 
+  select(CU)
+panel.ts <- function(data, mapping){
+  # print(data$BroodYear)
+  # print(data[,mapping$x])
+  x <- unique(unlist(lapply(mapping, all.vars)))
+  # print(x)
+  # print(data[,x])
+  new_data <- cbind(values = data[,x], time=data$BroodYear)
+  # print(x)
+  ggplot(new_data, aes(x = time, y = values))+
+    geom_line(size = 1.2, alpha = 0.5, color = "#C9C5BA")
+}
+
+ggpairs(chum_sst %>% 
+          filter(CU %in% cu_list$CU) %>% 
+         select(ECA_age_proxy_forested_only, meanSST, npgo, disturbedarea_prct_cs ,CU),
+        columnLabels =  c("ECA","SST","NPGO","CPD"),
+
+        axisLabels = "external", columns = 1:4, aes(alpha = 0.5, color = CU),
+        title = "Correlation plot between ECA, SST, NPGO, CPD for CUs with >10 rivers")+
+  scale_color_brewer(type = "qual", palette = "Set2")+
+  theme_classic()+
+  theme(strip.background = element_rect(fill = "white", color = "white"),
+        strip.text.x.top = element_text(size = 12),
+        strip.text.y.right = element_text(size = 12))
+ggsave(here("figures","correlation_plot_sst_npgo.png"), width = 12, height = 10)
+
